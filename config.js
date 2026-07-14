d04e11cf46b6724e2f8818c81ff3f6caa82f5514
MINIShortpunk <maria.abeyratne@gmail.com>
2026-07-14 16:05:57 +0100

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = 'https://ayyikkfjbbckgoqbkctx.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://ayyikkfjbbckgoqbkctx.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_6YExXaIkMiYetWGgsasWBg_7ePczCTg';
