ef4fe4d65201486783f66d430b3ed0fe77ea973e
MINIShortpunk <maria.abeyratne@gmail.com>
2026-07-16 11:28:48 +0100

    Update Supabase anon key and client initialization

--- supabase.js
+++ supabase.js
@@ -1,8 +1,9 @@
 const SUPABASE_URL = "https://qusyglgevgyjaoasmjhz.supabase.co";
 
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_ANON_KEY =
+"sb_publishable_9ty8SXkUWakdGnSSWt3WeA_8TgRQY8x";
 
 const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
