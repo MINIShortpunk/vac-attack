5ed85616cfb789b85b7edff984d7d1103b1cd18b
MINIShortpunk <maria.abeyratne@gmail.com>
2026-07-16 14:58:06 +0100

    Update service worker cache name and structure

--- sw.js
+++ sw.js
@@ -1,25 +1,56 @@
-const CACHE = 'vac-attack-v1';
-const ASSETS = ['./', './index.html', './app.js', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'vacatory-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './config.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE)
+          .map(k => caches.delete(k))
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+
   if (e.request.method !== 'GET') return;
+
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return res;
-    }).catch(() => cached))
+    caches.match(e.request).then(cached => {
+
+      return cached || fetch(e.request)
+        .then(res => {
+
+          const copy = res.clone();
+
+          caches.open(CACHE)
+            .then(c => c.put(e.request, copy));
+
+          return res;
+
+        })
+        .catch(() => cached);
+
+    })
   );
+
 });
