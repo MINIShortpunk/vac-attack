018f704262fb0b384d9ca4a14a65884295b09f9c
MINIShortpunk <maria.abeyratne@gmail.com>
2026-07-16 11:42:15 +0100

    Refactor Supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,9 +1,15 @@
-const SUPABASE_URL = "https://qusyglgevgyjaoasmjhz.supabase.co";
+// ======================================
+// VacAttack
+// Supabase Configuration
+// ======================================
+
+const SUPABASE_URL =
+"https://qusyglgevgyjaoasmjhz.supabase.co";
 
 const SUPABASE_ANON_KEY =
 "sb_publishable_9ty8SXkUWakdGnSSWt3WeA_8TgRQY8x";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+const client = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 );
